9d00773149649413dc7f13765a297487337c65d2
Alexandre Santos <alexandre.santozz@gmail.com>
2021-01-11 17:37:20 +0000

    feat: add types and log

--- mod.ts
+++ mod.ts
@@ -1,4 +1,5 @@
 import { parse } from "https://deno.land/std@0.83.0/flags/mod.ts";
+import { WeatherResponse } from "./types.ts";
 
 const { city } = parse(Deno.args);
 
@@ -7,8 +8,16 @@ if (!API_KEY) {
   throw new Error("OWM_API_KEY should be defined");
 }
 
-const response = await fetch(
+const response: WeatherResponse = await fetch(
   `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`,
 ).then((r) => r.json());
 
-console.log(response);
+console.log(`Weather for ${city}: ${response.weather.map((entry) => entry.main)}
+
+Temperature:
+  Max: ${response.main.temp_max}
+  Min: ${response.main.temp_min}
+  Feeling: ${response.main.feels_like}
+Wind:
+  Speed: ${response.wind.speed} km/h
+`);
